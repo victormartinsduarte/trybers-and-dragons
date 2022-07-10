81e7c150d785c7581aff25979879aabaf4ef48a3
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-09 22:08:55 -0300

    Feat: Req-02 Created Dwarf class extending from Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -22,15 +22,3 @@ abstract class Race {
 }
 
 export default Race;
-// import Fighter from '../Fighter';
-
-// abstract class Battle {
-//   constructor(protected player: Fighter) { }
-
-//   // Should return 1 if player wins, -1 otherwise
-//   fight(): number {
-//     return this.player.lifePoints === -1 ? -1 : 1;
-//   }
-// }
-
-// export default Battle;
