aaf5f0e5041465b46c84bbf454267a71d287709c
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-09 22:15:33 -0300

    Feat: Req-02 Created Halfling class extending from Race

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,6 +1,7 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfling';
 
-export { Dwarf, Elf };
+export { Dwarf, Elf, Halfling };
 export default Race;
