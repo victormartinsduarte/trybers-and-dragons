8842bcfd56a1514adb9992eb5be198c4b9772906
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-09 22:17:26 -0300

    Feat: Req-02 Created Orc class extending from Race

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -2,6 +2,7 @@ import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
 import Halfling from './Halfling';
+import Orc from './Orc';
 
-export { Dwarf, Elf, Halfling };
+export { Dwarf, Elf, Halfling, Orc };
 export default Race;
