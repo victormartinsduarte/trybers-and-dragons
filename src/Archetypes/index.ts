7a0bd243dcb7d2a0bc47d0e4cf9a9c06436fa6e5
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-10 17:23:04 -0300

    Feat: Req-05 Created class Warrior extending from Archetype

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,6 +1,7 @@
 import Archetype from './Archetype';
 import Mage from './Mage';
 import Necromancer from './Necromancer';
+import Warrior from './Warrior';
 
-export { Mage, Necromancer };
+export { Mage, Necromancer, Warrior };
 export default Archetype;
