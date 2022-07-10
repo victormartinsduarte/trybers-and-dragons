88fc58fe2c3b4d5848e6d644ff6766fb5087d132
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-09 21:34:15 -0300

    Chore: Refac of class Race to use private attributes name convention

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,5 +1,18 @@
 abstract class Race {
-  constructor(readonly name: string, readonly dexterity: number) {}
+  private _name: string;
+  private _dexterity: number;
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
 
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
